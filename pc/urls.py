ec284c04084135e88f00319a483396b05e607e4c
QWR3 <anton004@protonmail.com>
2021-09-05 13:50:05 +0300

    HW2

--- pc/urls.py
+++ pc/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import PC_list_get_create, PC_read_update_delete
 
 urlpatterns = [
-    path('', PC_list_get_create.as_view()),
-    path('<int:pk>/', PC_read_update_delete.as_view()),
+    path('', PC_list_get_create.as_view(), name='pc_list_create'),
+    path('<int:pk>/', PC_read_update_delete.as_view(), name='pc_retrieve_update_delete'),
 ]
